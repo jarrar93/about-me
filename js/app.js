0395452ca2d9905bb66c479973f1d7d24232b07b
jarrar93 <mohammadjarrar810@yahoo.com>
2021-04-26 06:15:52 -0700

    lap 02 " about me"

---
+++ js/app.js
@@ -0,0 +1,63 @@
+let name;
+name = prompt('Hello, whats\'s your name ?');
+alert('welcome ' + " " + name + '  in my page');
+alert('I will ask you some questions about me, please answer yes or no');
+
+
+let name1 = prompt("am i student?");
+
+if (name1.toLowerCase() == "yes" || name1.toLowerCase() == "y") {
+  alert('yes i am student')
+}
+else if (name1.toLowerCase() == "no" || name1.toLowerCase() == "n") {
+  alert(" no i am  student")
+} else {
+  alert('please enter yes or no');
+
+}
+let age = prompt("Is my age 28 ?")
+if (age.toLowerCase() == "yes" || age.toLowerCase() == "y") {
+  alert(" yes i am 28 years old ")
+} else if (age.toLowerCase() == "no" || age.toLowerCase() == "n") {
+  alert('no it is false answer')
+} else {
+  alert('please enter yes or no')
+}
+let live = prompt("Do I live in Zarqa? ")
+
+if (live.toLowerCase() == "yes" || live.toLowerCase() == "y") {
+  alert(" NO i live in amman ")
+} else if (live.toLowerCase() == "no" || live.toLowerCase() == "n") {
+  alert('exactly  your answer')
+} else {
+  alert('please enter yes or no')
+}
+let num = prompt("Am I a doctor?")
+
+if (num.toLowerCase() == "yes" || num.toLowerCase() == "y") {
+  alert(" NO i am not a doctor ")
+} else if (num.toLowerCase() == "no" || num.toLowerCase() == "n") {
+  alert('your answer is corect')
+} else {
+  alert('please enter yes or no')
+}
+let app = prompt("Am I currently learning programming? ")
+
+if (app.toLowerCase() == "yes" || app.toLowerCase() == "y") {
+  alert(" exactly  ")
+} else if (app.toLowerCase() == "no" || app.toLowerCase() == "n") {
+  alert('no i learning this ')
+} else {
+  alert('please enter yes or no')
+
+}
+alert("nice to meet you  " + name);
+
+
+
+
+
+// YES
+// yes
+//          yes
+// if (x.toLowerCase()=="yes" || x.toLowerCase()=='y')
