9a4dc73f86f626640f392f3a90cb16bc7b2aad80
jarrar93 <mohammadjarrar810@yahoo.com>
2021-04-29 13:04:08 -0700

    end work

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# about-me
+i fixed my code in js file , because i have alot of problem .
+i will puting all codes in function
--- js/app.js
+++ js/app.js
@@ -1,168 +1,150 @@
 
+'ues stract';
+
 let name;
+let scoor = 0;
+
+
 name = prompt('My name is Ahmed, and I am happy that you visited my page. Can I know your name? ?');
 alert('welcome ' + " " + name + '  in my page');
 alert('I will ask you some questions about me, please answer yes or no');
 
 
-let name1 = prompt("am i student?");
 
-if (name1.toLowerCase() == "yes" || name1.toLowerCase() == "y") {
-  alert('yes i am student')
-}
-else if (name1.toLowerCase() == "no" || name1.toLowerCase() == "n") {
-  alert(" no i am  student")
-} else {
-  alert('please enter yes or no');
+// first 4 Question :-
 
-}
-let age = prompt("Is my age 28 ?")
-if (age.toLowerCase() == "yes" || age.toLowerCase() == "y") {
-  alert(" yes i am 28 years old ")
-} else if (age.toLowerCase() == "no" || age.toLowerCase() == "n") {
-  alert('no it is false answer')
-} else {
-  alert('please enter yes or no')
-}
-let live = prompt("Do I live in Zarqa? ")
+function yesNO() {
 
-if (live.toLowerCase() == "yes" || live.toLowerCase() == "y") {
-  alert(" NO i live in amman ")
-} else if (live.toLowerCase() == "no" || live.toLowerCase() == "n") {
-  alert('exactly  your answer')
-} else {
-  alert('please enter yes or no')
-}
-let num = prompt("Am I a doctor?")
 
-if (num.toLowerCase() == "yes" || num.toLowerCase() == "y") {
-  alert(" NO i am not a doctor ")
-} else if (num.toLowerCase() == "no" || num.toLowerCase() == "n") {
-  alert('your answer is corect')
-} else {
-  alert('please enter yes or no')
-}
-let app = prompt("Am I currently learning programming? ")
+  let name1 = prompt("am i student?");
 
-if (app.toLowerCase() == "yes" || app.toLowerCase() == "y") {
-  alert(" exactly  ")
-} else if (app.toLowerCase() == "no" || app.toLowerCase() == "n") {
-  alert('no i learning this ')
-} else {
-  alert('please enter yes or no')
-
-}
-
-alert('I will ask you a question and I want you to guess the correct answer,OK');
-alert('You only have 4 attempts to answer')
-let questions;
-
-for (i = 1; i <= 4; i++) {
-
-  questions = Number(prompt('How old was the Prophet Muhammad when he died..? '))
-
-  if (questions == 63) {
-
-    alert('yes this is corect answer ')
-    break;
-  } else if (questions <= 62) {
-    alert(' your answer it\'s too low')
-
-  } else if (questions >= 64) {
-    alert(' your answer it\'s too high')
-  } while (i >= 4) {
-    alert(' the corect asnwer is 63 yaers')
-    break;
+  if (name1.toLowerCase() == "yes" || name1.toLowerCase() == "y") {
+    alert('yes i am student')
+    scoor++;
   }
+  else if (name1.toLowerCase() == "no" || name1.toLowerCase() == "n") {
+    alert(" no i am  student")
+  } else {
+    alert('please enter yes or no');
 
+  }
+  let age = prompt("Is my age 28 ?")
+  if (age.toLowerCase() == "yes" || age.toLowerCase() == "y") {
+    alert(" yes i am 28 years old ")
+    scoor++;
+  } else if (age.toLowerCase() == "no" || age.toLowerCase() == "n") {
+    alert('no it is false answer')
+  } else {
+    alert('please enter yes or no')
+  }
+  let live = prompt("Do I live in Zarqa? ")
 
+  if (live.toLowerCase() == "yes" || live.toLowerCase() == "y") {
+    alert(" NO i live in amman ")
 
-}
+  } else if (live.toLowerCase() == "no" || live.toLowerCase() == "n") {
+    alert('exactly  your answer')
+    scoor++;
+  } else {
+    alert('please enter yes or no')
+  }
+  let num = prompt("Am I a doctor?")
 
-alert('Hi , i will to ask you anther question,And you have 6 attempts, if you know one of the correct answers, you will be the winner....OK ');
-let multi = ['Norway', 'Denmark', 'Iceland', 'Sweden', 'Finland', 'Faroe Islands']
+  if (num.toLowerCase() == "yes" || num.toLowerCase() == "y") {
+    alert(" NO i am not a doctor ")
 
-let answer = prompt('Mention a country from Scandinavia...?')
-let x;
+  } else if (num.toLowerCase() == "no" || num.toLowerCase() == "n") {
+    alert('your answer is corect')
+    scoor++;
+  } else {
+    alert('please enter yes or no')
+  }
+  let app = prompt("Am I currently learning programming? ")
 
-for (i = 1; i <= 5; i++) {
-
-  if (answer.toLowerCase() == "norway" || answer.toUpperCase() == "Norway") {
-    alert(' exactly this contry form Scandinavia')
-
-    break;
-
-
-  } else if (answer.toLowerCase() == "denmark" || answer.toUpperCase() == "Denmark") {
-    alert('exactly this contry form Scandinavia')
-
-    break;
-
-  } else if (answer.toLowerCase() == "iceland" || answer.toUpperCase() == "Iceland") {
-    alert('exactly this contry form Scandinavia')
-    break;
-
-  } else if (answer.toLowerCase() == "sweden" || answer.toUpperCase() == "Sweden") {
-    alert('exactly this contry form Scandinavia')
-
-    break;
-  } else if (answer.toLowerCase() == "finland" || answer.toUpperCase() == "Finland") {
-    alert('exactly this contry form Scandinavia')
-
-    break;
-  } else if (answer.toLowerCase() == "faroe islands" || answer.toUpperCase() == "Faroe Islands") {
-    alert('exactly this contry form Scandinavia')
-    break;
-
-  } else if (answer != 1) {
-
-    alert(' no answer')
-    answer = prompt('Mention a country from Scandinavia...?')
+  if (app.toLowerCase() == "yes" || app.toLowerCase() == "y") {
+    alert(" exactly  ")
+    scoor++;
+  } else if (app.toLowerCase() == "no" || app.toLowerCase() == "n") {
+    alert('no i learning this ')
+  } else {
+    alert('please enter yes or no')
 
   }
 }
 
 
 
+// sixth Question :-
+
+function prophet() {
+
+  alert('I will ask you a question and I want you to guess the correct answer,OK');
+  alert('You only have 4 attempts to answer')
+  let questions;
+  for (i = 1; i <= 4; i++) {
+
+    questions = Number(prompt('How old was the Prophet Muhammad when he died..? '))
+
+    if (questions == 63) {
+
+      alert('yes this is corect answer ')
+      scoor++;
+      break;
+
+    } else if (questions <= 62) {
+      alert(' your answer it\'s too low')
+
+    } else if (questions >= 64) {
+      alert(' your answer it\'s too high')
+
+    } while (i == 4) {
+      alert(' the corect asnwer is 63 yaers')
+      break;
+    }
 
 
 
+  }
+}
+
+//7th Question:- 
+
+function contrys() {
+  alert('Hi , i will to ask you anther question,And you have 6 attempts, if you know one of the correct answers, you will be the winner....OK ');
+  alert(' i will to show you your gride in this Question , just you have 6 Guesses ')
 
 
+  let multi = ["Norway", "Denmark", "Iceland", "Sweden", "Finland", "Faroe Islands"]
+
+  for (let j = 0; j < 7; j++) {
+
+    for (let i = 0; i < multi.length; i++) {
+
+      let answer = prompt('Mention a country from Scandinavia...?')
+
+      if (answer == multi[i]) {
+        alert(' yes this is corect answer');
+        console.log(multi[i]);
+        scoor++;
+        j = 7;
+        break;
+      } else {
+        alert('no it is wrong answer, Try agin')
+        j++;
+      }
+
+    }
+  }
+}
 
 
+// calls the function:-
+yesNO();
+prophet();
+contrys();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+alert(' your grade is ' + scoor + " / 7")
 alert("nice to meet you  " + name);
 
 
